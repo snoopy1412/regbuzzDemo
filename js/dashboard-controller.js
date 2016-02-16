a937d76b5c268301a388a66033100b0ab2da6fff
snoopy1412 <402090261@qq.com>
2016-02-16 14:43:54 +0800

    modified project-list.html~~~~~~~~~~

--- js/dashboard-controller.js
+++ js/dashboard-controller.js
@@ -1,3 +1,42 @@
 define(['Vue'],function(Vue){
-
+	
+var dashBoardVm = new Vue({
+		el:"#vue-home",
+		data:{
+			roles:[
+				{
+					name : 'employer',
+					context:{
+						project :{
+							title:'发布项目',
+							num:10
+						},
+						question:{
+							title:'发布问题',
+							num:10
+						}
+					}
+				},
+				{
+					name : "provider",
+					context:{
+						project :{
+							title:'投标项目',
+							num:10
+						},
+						question:{
+							title:'问题回答',
+							num:10
+						}
+					}
+				}
+			],
+			autoindex : 0
+		},
+		methods:{
+			changeView:function(index){
+				this.autoindex = index;
+			}
+		}
+	})
 });
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,18 +1,4 @@
 define(['./js/dashboard-controller.js'],function(){
 
-  // 角色转换
-  $('#change-role input[type=radio]').on('change',function(){
 
-    var employerInput = $('#employerView_option').prop('checked'),
-        providerInput = $('#providerView_option').prop('checked');  
-    console.log(employerInput,providerInput);
-    if(employerInput && !providerInput){
-      $('.history-content .employer-view').addClass('active');
-      $('.history-content .provider-view').removeClass('active');
-    }
-    if(providerInput && !employerInput){
-      $('.history-content .employer-view').removeClass('active');
-      $('.history-content .provider-view').addClass('active');
-    }
-  })
 });
