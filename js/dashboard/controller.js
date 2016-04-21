b7f08a8e8d3c33ba304c0520f49db14c6fa2333d
snoopy1412 <402090261@qq.com>
2016-04-21 14:49:11 +0800

    细节修正

--- js/dashboard/controller.js
+++ js/dashboard/controller.js
@@ -1,19 +1,19 @@
 define(['Vue', 'VueComponent'], function(Vue, VueComponent) {
 
 	var homehistory = VueComponent.homehistory;
-
+	var currentIndex;
 	// 全局注册fadeIn
 	Vue.transition('pushskip', {
 		afterLeave: function(el, done) {
 
-			// 这里是一个明显的ajax操作
-			dashbordvm.pushData.push({
+			// 模拟ajax操作
+			dashbordvm.pushData.splice(currentIndex,0,{
 				projectID: 23, //模拟
 				category: {
 					first: 'aaa',
 					sub: 'aaa'
 				},
-				title: '这是一条模拟数据',
+				title: '这是一条模拟数据'+ Math.random(),
 				price: {
 					num: 100000,
 					currency: 'USD'
@@ -101,7 +101,8 @@ define(['Vue', 'VueComponent'], function(Vue, VueComponent) {
 					remaining: '5',
 					description: 'I need a new website. I need you to design and build my online store. I will be selling clothes, front page will have pics and brief info of clothes then once the click item will go to next specific webpage for individual item											I need a new website. I need you to design and build my online store. I will be selling clothes, front page will have pics and brief info of clothes then once the click item will go to next specific webpage for individual item'
 				}
-			]
+			],
+			saveHide : false
 		},
 		methods: {
 			pushSkip: function(index, event) {
@@ -109,7 +110,16 @@ define(['Vue', 'VueComponent'], function(Vue, VueComponent) {
 				// 即发起ajax请求。
 				// $.ajax(url,data,success);
 				// 这里我们进行模拟
+				currentIndex = index;
 				this.pushData.splice(index, 1);
+			},
+			favorite : function(index , event){
+				// 收藏操作
+				// 由于我们在前面收集了projectID，所以这里可以考虑只储存projectID即可
+				
+				// 其他模拟
+				this.saveHide = true
+				
 			}
 		}
 	});
